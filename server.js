8b2afdd131eff3e9be53beeee3c1123f3f4034e7
Philip Rosen <philipjrosen@gmail.com>
2014-09-15 21:27:41 -0700

    moved router declaration to top

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var router = require('./app/routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
-
-var router = require('./app/routes');
 
 app.use('/', router);
 
