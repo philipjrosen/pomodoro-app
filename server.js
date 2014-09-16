0e2caf0c0f49e6b569b0a8daa585577bee5ae758
Philip Rosen <philipjrosen@gmail.com>
2014-09-15 20:51:24 -0700

    moved express.Router into routes.js in app folder

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-var router = express.Router();
-
-router.get('/', function(req, res) {
-  res.json({ message: 'basic api working' }); 
-});
+var router = require('./app/routes');
 
 app.use('/', router);
 
