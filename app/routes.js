b7bcf0e2d58cb83eb045abfe6a3d872ba4a1d5ac
Philip Rosen <philipjrosen@gmail.com>
2014-09-15 22:28:18 -0700

    adds post to routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var ToDo = require('./models/todo');
 
 router.get('/', function(req, res) {
   res.json({ message: 'basic api working' }); 
 });
+
+router.route('/todo')
+
+  .post(function(req, res){
+    var todo = new ToDo();
+    todo.name = req.body.name;
+
+    todo.save(function(err) {
+      if(err){
+        res.send(err);
+      }
+
+      res.json({message: "ToDo Created"});
+
+    });
+  });
 
 module.exports = router;
 
