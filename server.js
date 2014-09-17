cfa52fc1f34ffdcf5bc43e32e26a2a708e4bc70d
Philip Rosen <philipjrosen@gmail.com>
2014-09-17 13:25:32 -0700

    moved http routing to server.js; no longer uses routes.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var router = require('./app/routes');
+
+var ToDo = require('./app/models/todo');
+
 
 var db = require('./config/db');
 
@@ -21,6 +23,71 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
+
+//Handle the routes for http requests from angular
+var router = express.Router();
+
+router.route('/todos')
+
+  .post(function(req, res){
+    var todo = new ToDo();
+    todo.name = req.body.name;
+
+    todo.save(function(err) {
+      if(err){
+        res.send(err);
+      }
+
+      res.json({message: "ToDo Created"});
+
+    });
+  })
+  .get(function(req, res) {
+    ToDo.find(function(err, todos) {
+      if (err)
+        res.send(err);
+
+      res.json(todos);
+    });
+  });
+
+router.route('/todos/:todo_id')
+
+  .get(function(req, res) {
+    ToDo.findById(req.params.todo_id, function(err, todo) {
+      if (err)
+        res.send(err);
+      res.json(todo);
+    });
+  })
+  .put(function(req, res) {
+
+    ToDo.findById(req.params.todo_id, function(err, todo) {
+
+      if (err)
+        res.send(err);
+
+      todo.name = req.body.name;
+
+      todo.save(function(err) {
+        if (err)
+          res.send(err);
+
+        res.json({ message: 'ToDo updated!' });
+      });
+    });
+  })
+  .delete(function(req, res) {
+    ToDo.remove({
+      _id: req.params.todo_id
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
+
 app.use('/', router);
 
 app.listen(port);
